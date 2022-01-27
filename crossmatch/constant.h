bb6a7ccee2e11479f9528a8fde6bbe5cf8fb53ad
Melody <1878626518@qq.com>
2022-01-27 17:50:39 +0800

    Update constant.h

--- crossmatch/constant.h
+++ crossmatch/constant.h
@@ -19,8 +19,6 @@
 
 using namespace std;
 
-
-/*两个星表的误差半径，与具体算法直接相关，这里取的是1角秒(即角度单位中的1秒)*/
 #define R_A 0.001388889/5
 #define R_B 0.001388889/5
 #define RADIANS 57.29577951308232311024
@@ -28,7 +26,6 @@ using namespace std;
 #define PI 3.1415926535
 #define DIS 9.0*(R_A*R_A+R_B*R_B)
 
-/*改成自动计算的 不需要手动计算 再输入 */
 #define N (unsigned long)(1<<19) // 524,288
 //#define SHARED_Y (1<<11) //2048
 #define BLOCK_MAX_X (1<<15) //32,768
