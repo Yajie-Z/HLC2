a3eb5c6bf08e3b257f929756589f156248169688
Melody <1878626518@qq.com>
2022-06-19 21:13:05 +0800

    Update constant.h

--- crossmatch/constant.h
+++ crossmatch/constant.h
@@ -16,18 +16,12 @@
 #include <string>
 #include <atomic> 
 
-
 using namespace std;
 
+//constant values used in the calculation of the spherical angle distance
 #define R_A 0.001388889/5
 #define R_B 0.001388889/5
 #define RADIANS 57.29577951308232311024
 #define D2R  (1.7453292519943295769e-2)
 #define PI 3.1415926535
 #define DIS 9.0*(R_A*R_A+R_B*R_B)
-
-//#define N (unsigned long)(1<<19) // 524,288
-//#define SHARED_Y (1<<11) //2048
-//#define BLOCK_MAX_X (1<<15) //32,768
-//#define BLOCK_MAX_Y (1<<14) //16,384
-
