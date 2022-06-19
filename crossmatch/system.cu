93b7da5cfdc7f780cedd891398face7b03e697df
Melody <1878626518@qq.com>
2022-06-19 21:28:15 +0800

    Update system.cu

--- crossmatch/system.cu
+++ crossmatch/system.cu
@@ -1,9 +1,11 @@
 #include "system.h"
-//source reading module
+
+//the implementation of source reading module
 double cpu_helper()
 {
     FILE *fp = NULL;
     FILE *fp2 = NULL;
+	
     fp=popen("cat /proc/meminfo | grep MemTotal:|sed -e 's/.*:[^0-9]//'","r");
     fp2=popen("cat /proc/meminfo | grep MemAvailable:|sed -e 's/.*:[^0-9]//'","r");
 
@@ -11,16 +13,13 @@ double cpu_helper()
     char buf2[100];
     double avaliableCPU;
     double totalCPU;
+	
     while(memset(buf, 0, sizeof(buf)), fgets(buf, sizeof(buf) - 1, fp) != 0 ) {
-      totalCPU = strtod(buf,NULL);
-      //printf("%s", buf);
-      //printf( "%f\n", totalCPU );
+    	totalCPU = strtod(buf,NULL);
     }
     
     while(memset(buf2, 0, sizeof(buf)), fgets(buf2, sizeof(buf2) - 1, fp2) != 0 ) {
-      //printf("%s", buf2);
-      avaliableCPU = strtod(buf2,NULL);
-      //printf( "%f\n", avaliableCPU);
+ 	avaliableCPU = strtod(buf2,NULL);
     }
 
     pclose(fp);
@@ -34,6 +33,7 @@ vector<double> gpu_helper()
 	size_t free_byte;
 	size_t total_byte;
 	cudaError_t cuda_status = cudaMemGetInfo(&free_byte, &total_byte);
+	
 	if (cudaSuccess != cuda_status) {
 		printf("Error: cudaMemGetInfo fails, %s \n", cudaGetErrorString(cuda_status));
 		exit(1);
